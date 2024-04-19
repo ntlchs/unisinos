2b581b61d198e67a728910b8e9ab2c570d2c39c3
ntlchs <nataliachies@gmail.com>
2024-04-19 01:03:51 -0300

    fix

--- algoritmos-mod3-2024/Main.java
+++ algoritmos-mod3-2024/Main.java
@@ -1,96 +1,58 @@
 public class Main {
 
   public static void main(String[] args) {
-    testIntegerList();
-    testStringList();
-    testDoubleList();
-    testCharacterList();
-    testBooleanList();
-    testArrayList();
-    testNoObject();
-    testBracketChecking();
+    testElementCount(new Integer[] { 1, 2, 3, 1, 5, 1 }, 1);
+    testElementCount(new String[] { "a", "b", "c", "a", "e", "a" }, "a");
+    testElementCount(new Double[] { 1.0, 2.0, 3.0, 1.0, 1.0 }, 1.0);
+    testElementCount(new Character[] { 'a', 'b', 'c', 'a', 'e', 'a' }, 'a');
+    testElementCount(new Boolean[] { true, false, true, true, false }, true);
+
+    testArrayCount();
+    testObjectCount();
     testOverflowException();
     testUnderflowException();
+
+    testBracketChecking();
   }
 
-  private static void testIntegerList() {
-    Metodo1<Integer> metodo1 = new Metodo1<>(10);
-    Integer[] numbers = { 1, 2, 3, 1, 5, 1, 7, 10, 9, 0 };
-    metodo1.initList(numbers);
-    int count = metodo1.contaElementos(1);
-    System.out.println("number 1: " + count);
+  private static <T> void testElementCount(T[] elements, T target) {
+    Metodo1<T> metodo1 = new Metodo1<>(elements.length);
+    metodo1.initList(elements);
+    int count = metodo1.contaElementos(target);
+    System.out.println("Count of " + target + ": " + count);
   }
 
-  private static void testStringList() {
-    Metodo1<String> metodo1String = new Metodo1<>(10);
-    String[] strings = { "a", "b", "c", "a", "e", "a", "b", "c", "d", "e" };
-    metodo1String.initList(strings);
-    int countString = metodo1String.contaElementos("a");
-    System.out.println("string 'a': " + countString);
-  }
-
-  private static void testDoubleList() {
-    Metodo1<Double> metodo1Double = new Metodo1<>(10);
-    Double[] doubles = { 1.0, 2.0, 3.0, 1.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0 };
-    metodo1Double.initList(doubles);
-    int countDouble = metodo1Double.contaElementos(1.0);
-    System.out.println("number 1.0: " + countDouble);
-  }
-
-  private static void testCharacterList() {
-    Metodo1<Character> metodo1Char = new Metodo1<>(10);
-    Character[] chars = { 'a', 'b', 'c', 'a', 'e', 'a', 'b', 'c', 'd', 'e' };
-    metodo1Char.initList(chars);
-    int countChar = metodo1Char.contaElementos('a');
-    System.out.println("char 'a': " + countChar);
-  }
-
-  private static void testBooleanList() {
-    Metodo1<Boolean> metodo1Boolean = new Metodo1<>(10);
-    Boolean[] booleans = {
-      true,
-      false,
-      true,
-      true,
-      false,
-      false,
-      false,
-      false,
-      false,
-      false,
-    };
-    metodo1Boolean.initList(booleans);
-    int countBoolean = metodo1Boolean.contaElementos(true);
-    System.out.println("true: " + countBoolean);
-  }
-
-  private static void testArrayList() {
-    Metodo1<int[]> metodo1Array = new Metodo1<>(10);
+  private static void testArrayCount() {
+    Metodo1<int[]> metodo1Array = new Metodo1<>(5);
     int[] arr1 = { 1, 2, 3 };
     int[] arr2 = { 1, 2, 4 };
-    int[][] arrays = {
-      arr1,
-      arr1,
-      arr2,
-      arr1,
-      arr2,
-      arr2,
-      arr2,
-      arr2,
-      arr2,
-      arr2,
-    };
+    int[][] arrays = { arr1, arr1, arr2, arr1, arr2 };
     metodo1Array.initList(arrays);
     int countArr = metodo1Array.contaElementos(arr1);
-    System.out.println("array [1, 2, 3]: " + countArr);
+    System.out.println("Count of array [1, 2, 3]: " + countArr);
   }
 
-  private static void testNoObject() {
-    Metodo1<Object> metodo1Object = new Metodo1<>(3);
-    Object[] objects = { new Object(), new Object(), new Object() };
-    metodo1Object.initList(objects);
-    int countObject = 0;
-    System.out.println("object: " + countObject);
+  private static void testObjectCount() {
+    Metodo1<Object> metodo1Obj = new Metodo1<>(5);
+    Object obj1 = new Object();
+    Object obj2 = new Object();
+    Object[] objs = { obj1, obj2, obj1, obj1, obj2 };
+    metodo1Obj.initList(objs);
+    int countObj = metodo1Obj.contaElementos(obj1);
+    System.out.println("Count of object Obj(1): " + countObj);
+  }
+
+  private static void testOverflowException() {
+    StaticQueue<Integer> queue = new StaticQueue<>(3);
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.enqueue(4);
+  }
+
+  public static void testUnderflowException() {
+    StaticQueue<Integer> queue = new StaticQueue<>(3);
+    queue.dequeue();
   }
 
   private static void testBracketChecking() {
@@ -108,23 +70,9 @@ public class Main {
         expressionStack.push(expr.charAt(i));
       }
 
-      String isValid =
-        (metodo2.checkBrackets(expressionStack) ? "valid" : "invalid");
+      boolean isValid = metodo2.checkBrackets(expressionStack);
 
-      System.out.println(expr + ": " + isValid + " ()");
+      System.out.println(expr + ": " + (isValid ? "valid" : "invalid") + " ()");
     }
-  }
-
-  private static void testOverflowException() {
-    StaticQueue<Integer> queue = new StaticQueue<>(3);
-    queue.enqueue(1);
-    queue.enqueue(2);
-    queue.enqueue(3);
-    queue.enqueue(4);
-  }
-
-  public static void testUnderflowException() {
-    StaticQueue<Integer> queue = new StaticQueue<>(3);
-    queue.dequeue();
   }
 }
