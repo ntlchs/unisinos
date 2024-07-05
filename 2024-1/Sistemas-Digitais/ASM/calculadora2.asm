6852b38959752dde18af305667009325696c6b09
ntlchs <nataliachies@gmail.com>
2024-07-05 01:09:46 -0300

    .

--- 2024-1/Sistemas-Digitais/ASM/calculadora2.asm
+++ 2024-1/Sistemas-Digitais/ASM/calculadora2.asm
@@ -8,18 +8,18 @@
 # Exemplo: li $v0, 4       # carrega valor 4 no registrador $v0
 
 # la: load address
-# la <registrador>, <rótulo>
-# Exemplo: la $a0, msg     # carrega endereço da string `msg` no registrador $a0
+# la <registrador>, <rÃ³tulo>
+# Exemplo: la $a0, msg     # carrega endereÃ§o da string `msg` no registrador $a0
 
 # syscall: chamada de sistema
 # syscall
-# Exemplo: syscall         # executa chamada de sistema com código em $v0
+# Exemplo: syscall         # executa chamada de sistema com cÃ³digo em $v0
 
 # jal: jump and link
-# jal <rótulo>
-# Exemplo: jal le_inteiro_do_teclado  # salta para rótulo `le_inteiro_do_teclado` e armazena endereço de retorno em $ra
+# jal <rÃ³tulo>
+# Exemplo: jal le_inteiro_do_teclado  # salta para rï¿½tulo `le_inteiro_do_teclado` e armazena endereÃ§o de retorno em $ra
 
-# move: move conteúdo entre registradores
+# move: move conteÃºdo entre registradores
 # move <destino>, <origem>
 # Exemplo: move $t0, $v0  # move valor do registrador $v0 para registrador $t0
 
@@ -41,8 +41,8 @@
 # Exemplo: div $t0, $t1       # divide $t0 por $t1
 
 # mflo: move from LO (valor do registrador especial LO para registrador geral)
-# mflo <destino>              # move quociente da divisão para <destino>
-# Exemplo: mflo $t5           # move quociente (resultado da divisão) para $t5
+# mflo <destino>              # move quociente da divisÃ£o para <destino>
+# Exemplo: mflo $t5           # move quociente (resultado da divisÃ£o) para $t5
 
 # mfhi: move from HI
 # mfhi <destino> 
@@ -58,23 +58,23 @@ msg_mul: .asciiz "Resultado da multiplicacao: "
 msg_div: .asciiz "Resultado da divisao: "
 
 .text
-# Instruções
+# instruÃ§Ãµes
 .globl inicio
 
 inicio:
-    # exibe mensagem e lê 1º número
-    li $v0, 4       # código para imprimir uma string
-    la $a0, msg     # carrega o endereço da mensagem em $a0
-    syscall         # chamada do sistema para $v0 que contém 4 (imprimir string)
-    jal le_inteiro_do_teclado  # chama função
+    # exibe mensagem e lÃª 1o nÃºmero
+    li $v0, 4       # cÃ³digo para imprimir uma string
+    la $a0, msg     # carrega o endereï¿½o da mensagem em $a0
+    syscall         # chamada do sistema para $v0 que contÃ©m 4 (imprimir string)
+    jal le_inteiro_do_teclado  # chama funÃ§Ã£oo
     move $t0, $v0  # armazena inteiro lido em $t0
     li $v0, 4          
     la $a0, msg2       
     syscall
-    move $a0, $t0   #a0 é o 1º srgumento de syscall
+    move $a0, $t0   # a0 Ã© o 10 argumento de syscall
     jal imprime_inteiro            
     
-    # exibe mensagem e lê 2º número
+    # exibe mensagem e lÃª 20 nÃºmero
     li $v0, 4       
     la $a0, msg     
     syscall         
@@ -86,7 +86,7 @@ inicio:
     move $a0, $t1
     jal imprime_inteiro              
     
-    # Soma
+    # soma
     add $t2, $t0, $t1  # soma $t0 e $t1 e armazena resultado em $t2
     li $v0, 4          
     la $a0, msg_soma   
@@ -94,7 +94,7 @@ inicio:
     move $a0, $t2   
     jal imprime_inteiro       
     
-    # Subtração
+    # subtraÃ§Ã£o
     sub $t3, $t0, $t1  # subtrai $t1 de $t0 e armazena resultado em $t3
     li $v0, 4          
     la $a0, msg_sub    
@@ -102,7 +102,7 @@ inicio:
     move $a0, $t3      
     jal imprime_inteiro          
     
-    # Multiplicação
+    # multiplicaÃ§Ã£o
     mul $t4, $t0, $t1  # multiplica $t0 por $t1 e armazena resultado em $t4
     li $v0, 4          
     la $a0, msg_mul    
@@ -110,26 +110,26 @@ inicio:
     move $a0, $t4      
     jal imprime_inteiro             
     
-    # Divisão
+    # divisÃ£o
     div $t0, $t1       # divide $t0 por $t1
     mflo $t5           # move quociente de $lo para $t2 
     li $v0, 4          
     la $a0, msg_div    
     syscall            
-    move $a0, $t5      # move o resultado da divisão para $a0
+    move $a0, $t5      # move o resultado da divisÃ£o para $a0
     jal imprime_inteiro             
 
-    # Fim do programa
+    # fim do programa
     li $v0, 10         # encerrar o programa
     syscall            # chamada do sistema para encerrar
 
-# funções
+# funÃ§Ãµees
 le_inteiro_do_teclado: 
     li $v0, 5         # ler inteiro
     syscall           # chamada do sistema para ler inteiro
-    jr $ra            # retorna endereço de retorno armazenado em $ra
+    jr $ra            # retorna endereÃ§o de retorno armazenado em $ra
     
 imprime_inteiro:
     li $v0, 1          # imprimir inteiro
     syscall            # chamada do sistema para imprimir inteiro
-    jr $ra             # retorna endereço de retorno armazenado em $ra
+    jr $ra             # retorna endereÃ§o de retorno armazenado em $ra
